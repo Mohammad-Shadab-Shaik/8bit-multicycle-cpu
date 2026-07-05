ee70ac437a5a228956d3955f8931268e2165b35a
Mohammad-Shadab-Shaik <75020195+Mohammad-Shadab-Shaik@users.noreply.github.com>
2026-07-05 17:09:20 +0530

    Add files via upload

---
+++ instruction_memory.v
@@ -0,0 +1,29 @@
+//this is an array of size 256 with each element being 16 bits wide
+module instruction_memory(
+    input [7:0] address,    //gets it from pc
+    output [15:0] instruction
+);
+
+reg [15:0] rom [0:255];     //array of 256 location of 16 bits width each
+
+initial begin       //Here, we prelead the ROM with my program. For testing, I'm inputting a small program that adds two numbers
+        rom[0] = 16'b0110_0000_0000_1010; // LDI AL, 10
+        rom[1] = 16'b0110_0010_0100_0101; // LDI BL, 5
+        rom[2] = 16'b0010_0001_0000_0000; // ADD AL, BL
+        rom[3] = 16'b0001_1000_0000_0000; // MOV CL, AL
+        rom[4] = 16'b0011_1001_0000_0000; // SUB CL, BL
+        rom[5] = 16'b0100_0001_0000_0000; // AND AL, BL
+        rom[6] = 16'b0101_1110_0000_0000; // OR  DL, CL
+        rom[7] = 16'b0000_0000_0000_0000; // NOP
+
+        begin : fill
+            integer i;
+            for(i=8; i<256; i =i +1)
+                rom[i] = 16'b0000_0000_0000_0000; //nop
+
+        end
+end
+
+assign instruction = rom[address];   //Assigning the output according to the address received by PC
+
+endmodule
---
+++ register_file.v
@@ -0,0 +1,28 @@
+module register_file(
+    input clk,
+    input reset,
+    input reg_write,    //allows destination register to store write_data during writeback
+    input [1:0] dest_reg_sel,
+    input [1:0] src_reg_sel,
+    input [7:0] write_data, //data that goes into the destination register (from alu or immediate value)
+    output [7:0] reg_data_dest, //current data in destination register
+    output [7:0] reg_data_src   //current data in source register
+);
+
+reg [7:0] registers[0:3];   //4 registers of 8 bits each
+always @(posedge clk)begin
+    if(reset)begin  //reset all general purpose registers to 0
+        registers[0] <= 8'h00;
+        registers[1] <= 8'h00;
+        registers[2] <= 8'h00;
+        registers[3] <= 8'h00;
+    end
+    else if (reg_write) begin   //sequential write logic
+        registers[dest_reg_sel] <= write_data;
+    end
+end
+
+//combinational read logic
+assign reg_data_dest = registers[dest_reg_sel];
+assign reg_data_src = registers[src_reg_sel];
+endmodule
